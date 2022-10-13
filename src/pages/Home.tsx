995a0e58499b4ed1c4ad2d2ab5fb7af239b56c53
Nathan Hui <hui.n1998@gmail.com>
2022-10-13 13:30:46 +0100

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,17 @@
+import Featured from "@components/home/Featured/Featured";
+import FeaturedCollection from "@components/home/FeaturedCollection/FeaturedCollection";
+import Prefooter from "@components/home/Prefooter/Prefooter";
+import Slideshow from "@components/home/Slideshow/Slideshow";
+import Testimonials from "@components/home/Testimonials/Testimonials";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <Slideshow />
+      <Testimonials />
+      <Featured />
+      <FeaturedCollection />
+      <Prefooter />
+    </main>
+  );
 }
