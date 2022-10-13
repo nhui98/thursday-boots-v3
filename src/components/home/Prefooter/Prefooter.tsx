c5ef7f2690d7f56c4ff42cf2640823b5e821c3a8
Nathan Hui <hui.n1998@gmail.com>
2022-10-13 13:23:01 +0100

    home page - prefooter section

---
+++ src/components/home/Prefooter/Prefooter.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+import { PREFOOTER_DATA } from "./data";
+
+export default function Prefooter() {
+  return (
+    <section className="bg-green py-16 text-white">
+      <div className="container mx-auto flex flex-col gap-y-8 px-8 text-center md:px-40 xl:px-80">
+        <h2 className="text-4xl font-bold">{PREFOOTER_DATA.title}</h2>
+        <p className="text-lg leading-8">{PREFOOTER_DATA.description}</p>
+        <div className="flex flex-col items-center justify-center gap-4 sm:flex-row">
+          {PREFOOTER_DATA.buttons.map(({ href, text }) => (
+            <Link
+              to={href}
+              key={text}
+              className="w-48 bg-white py-3 px-8 font-medium text-green hover:bg-white/80"
+            >
+              {text}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/home/Prefooter/data.ts
@@ -0,0 +1,15 @@
+export const PREFOOTER_DATA = {
+  title: "Why 'Thursday'?",
+  description:
+    "On Thursday we work hard, but it's also the unofficial start of the weekend, and we design our products with that versatility in mind. Your life isn't one-dimensional. Your footwear shouldn't be either.",
+  buttons: [
+    {
+      text: "SHOP MEN",
+      href: "/",
+    },
+    {
+      text: "SHOP WOMEN",
+      href: "/",
+    },
+  ],
+};
