d6b4653962f8db7874c8b500907b6b1d629d670e
Nathan Hui <hui.n1998@gmail.com>
2022-10-15 21:29:29 +0100

    feat: about - ourproduct compoenent

---
+++ src/pages/about/OurProduct.data.ts
@@ -0,0 +1,22 @@
+import OurProduct1 from "./images/OurProduct-1.webp";
+import OurProduct2 from "./images/OurProduct-2.webp";
+
+export const OUR_PRODUCT_DATA = {
+  title: "Concept Through Construction",
+  description:
+    "We believe in clean and timeless designs with no external branding, letting the high-quality materials and craftsmanship shine. Each of our products is completely designed and developed in-house, drawing inspiration from the world around us and our amazing customers. The result: products that are equal parts beautiful, functional and comfortable.",
+  feature1: {
+    img: OurProduct1,
+    alt: "boot design",
+    title: "Thoughtful Designs",
+    description:
+      "From the first design to robust product testing, we go the extra mile with you in mind. Whether it's custom leather tannages, world class rubber sole compounds, or little things like more durable eyelet rivets, every aspect of our footwear has been carefully considered. While this process isn't easy and is almost never noticed, making products we're proud of has always been our first priority.",
+  },
+  feature2: {
+    img: OurProduct2,
+    alt: "craftmanship",
+    title: "Expertly Handcrafted",
+    description:
+      "All of our footwear is handcrafted by some of the best artisans in North America and Europe. We are proud of our partners in all locations, as we ensure those we work with are able to meet our ethical sourcing criteria and have the technical know-how to make top quality footwear. Whether in León (Mexico), Porto (Portugal), Alicante (Spain), or Arkansas (USA), each pair of Thursdays is handcrafted in a time consuming and painstaking process with upward of 80 individual steps and an average production time of nearly 10 days per pair.",
+  },
+};
---
+++ src/pages/about/OurProduct.tsx
@@ -0,0 +1,19 @@
+import AboutTitle from "@components/about/AboutTitle/AboutTitle";
+import Feature from "@components/about/Feature/Feature";
+import Materials from "@components/about/Materials/Materials";
+
+import { OUR_PRODUCT_DATA } from "./OurProduct.data";
+
+export default function OurProduct() {
+  return (
+    <>
+      <AboutTitle
+        title={OUR_PRODUCT_DATA.title}
+        description={OUR_PRODUCT_DATA.description}
+      />
+      <Feature {...OUR_PRODUCT_DATA.feature1} />
+      <Materials />
+      <Feature {...OUR_PRODUCT_DATA.feature2} />
+    </>
+  );
+}
