98f8df0284c2631fc882713eef23598d1a0ce617
Nathan Hui <hui.n1998@gmail.com>
2022-10-15 20:33:22 +0100

    feat: about - our values component

---
+++ src/pages/about/OurValues.tsx
@@ -0,0 +1,18 @@
+import AboutTitle from "@components/about/AboutTitle/AboutTitle";
+import Feature from "@components/about/Feature/Feature";
+
+import { OUR_VALUES_DATA } from "./OurValues.data";
+
+export default function OurValues() {
+  return (
+    <>
+      <AboutTitle
+        title={OUR_VALUES_DATA.title}
+        description={OUR_VALUES_DATA.description}
+      />
+      {OUR_VALUES_DATA.features.map((feature) => (
+        <Feature key={feature.id} {...feature} />
+      ))}
+    </>
+  );
+}
