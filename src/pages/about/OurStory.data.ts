2702d9c5c3fce377dbc969f4d8506b838c432d9b
Nathan Hui <hui.n1998@gmail.com>
2022-10-15 18:55:15 +0100

    feat: OurStory component data and images

---
+++ src/pages/about/OurStory.data.ts
@@ -0,0 +1,53 @@
+import OurStory1 from "./images/OurStory-1.avif";
+import OurStory2 from "./images/OurStory-2.avif";
+import OurStory3 from "./images/OurStory-3.avif";
+import OurStory4 from "./images/OurStory-4.avif";
+import OurStory5 from "./images/OurStory-5.avif";
+
+export const OUR_STORY_DATA = {
+  title: "Comfort, Durability, Versatility & Honest Pricing.",
+  description:
+    "Thursday was built out of our own frustration. Clunky work boots or delicate fashion boots? Cheap shoes that fall apart after a few wears, or shoes that were incredibly overpriced? It didn’t seem right that we had to make these trade-offs. There had to be another option.",
+  features: [
+    {
+      id: 0,
+      img: OurStory1,
+      alt: "mens-captain-brown",
+      title: "Commitment To Quality",
+      description:
+        "Where other brands choose to cut corners, we invest. We select the best materials available for our footwear. From leather to laces, our products are thoughtfully designed and meticulously tested. The result: footwear that is as functional as it is fashionable.",
+    },
+    {
+      id: 1,
+      img: OurStory2,
+      alt: "",
+      title: "World-Class Partners",
+      description:
+        "We care deeply about how our products are made. That’s why we partner with world-renowned suppliers and highly skilled artisans to carefully handcraft our footwear. Only the highest ethical and quality standards are accepted. Our goal is to make products that we can proudly stand by, and that you can confidently stand in for years to come.",
+    },
+    {
+      id: 2,
+      img: OurStory3,
+      alt: "",
+      title: "A Better Experience",
+      description:
+        "The only thing we’re more passionate about than our products are the people who wear them. Whether you have a question, comment, or just want to tell us about your most recent #thursdayboots adventure (our favorite), our team is always here for you! After all, buying footwear should be fun, not stressful.",
+    },
+    {
+      id: 3,
+      img: OurStory4,
+      alt: "",
+      title: "For Wherever The Day Takes You",
+      description:
+        "'Thursday' is all about being ready for the unexpected. Officially a weekday, Thursday is also the unofficial start of the weekend. So no matter what you do for work, or where you go for fun, you can always stand confidently in your Thursdays. That is the mission behind every product we make. (And yes, you can wear them Friday through Wednesday, too.)",
+    },
+    {
+      id: 4,
+      img: OurStory5,
+      alt: "",
+      title: "8 Years Later",
+      description:
+        "It all started with a simple idea on a surf trip in Nicaragua to make the best boots possible and sell them at the best price possible. Today, Thursdays are being worn in all 50 states and more than 60 countries. What started as a 'bootstrapped' passion project has evolved into something truly special. It’s been a dream come true — and we’re just getting started.",
+    },
+  ],
+};
