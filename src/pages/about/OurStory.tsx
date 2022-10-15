066e6d4396248996e70014c09df6ef94add89cf7
Nathan Hui <hui.n1998@gmail.com>
2022-10-15 19:33:22 +0100

    feat: OurStory page

---
+++ src/pages/about/OurStory.tsx
@@ -0,0 +1,18 @@
+import AboutTitle from "@components/about/AboutTitle/AboutTitle";
+import Feature from "@components/about/Feature/Feature";
+
+import { OUR_STORY_DATA } from "./OurStory.data";
+
+export default function OurStory() {
+  return (
+    <>
+      <AboutTitle
+        title={OUR_STORY_DATA.title}
+        description={OUR_STORY_DATA.description}
+      />
+      {OUR_STORY_DATA.features.map((feature) => (
+        <Feature key={feature.id} {...feature} />
+      ))}
+    </>
+  );
+}
