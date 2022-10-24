01cf34f7f74d93e2d5484e1bdece21b7a9bbb81b
Nathan Hui <hui.n1998@gmail.com>
2022-10-24 18:01:10 +0100

    feat: login and register form components

---
+++ src/components/account/LoginForm/LoginForm.tsx
@@ -0,0 +1,51 @@
+import FormInput from "@components/common/FormInput/FormInput";
+import { Form, Formik } from "formik";
+import { Link } from "react-router-dom";
+import * as y from "yup";
+
+interface LoginFormValues {
+  email: string;
+  password: string;
+}
+
+export default function LoginForm() {
+  const initialValues: LoginFormValues = { email: "", password: "" };
+
+  const validationSchema = y.object().shape({
+    email: y.string().email("Invalid email").required("Required"),
+    password: y.string().required("Required"),
+  });
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      <Form className="flex w-full flex-col gap-y-8">
+        <FormInput
+          name="email"
+          label="Email"
+          type="email"
+          placeholder="example@email.com"
+        />
+
+        <FormInput name="password" label="Password" type="password" />
+
+        <div className="flex flex-wrap items-center gap-y-4 gap-x-4">
+          <button
+            type="submit"
+            className="bg-green px-8 py-3 tracking-widest text-white"
+          >
+            LOGIN
+          </button>
+          <Link to={"/account/recover"} className="text-sm text-black/50">
+            Forgot your password?
+          </Link>
+        </div>
+      </Form>
+    </Formik>
+  );
+}
---
+++ src/components/account/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,71 @@
+import FormInput from "@components/common/FormInput/FormInput";
+import { Form, Formik } from "formik";
+import * as y from "yup";
+
+interface RegisterFormValues {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export default function RegisterForm() {
+  const initialValues: RegisterFormValues = {
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const validationSchema = y.object().shape({
+    name: y
+      .string()
+      .required("Required")
+      .min(6, "Name must at least 6 characters")
+      .max(30, "Name cannot be greater than 30 characters"),
+    email: y.string().email("Invalid email").required("Required"),
+    password: y
+      .string()
+      .required("Required")
+      .min(8, "Password must be at least 8 characters"),
+    confirmPassword: y
+      .string()
+      .required("Required")
+      .oneOf([y.ref("password")], "Passwords do not match"),
+  });
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      <Form className="flex w-full flex-col gap-y-8">
+        <FormInput name="name" label="Name" type="text" />
+        <FormInput
+          name="email"
+          label="Email"
+          type="email"
+          placeholder="example@email.com"
+        />
+        <FormInput name="password" label="Password" type="password" />
+        <FormInput
+          name="confirmPassword"
+          label="Confirm Password"
+          type="password"
+        />
+
+        <div className="flex items-center gap-x-4">
+          <button
+            type="submit"
+            className="bg-green px-8 py-3 tracking-widest text-white"
+          >
+            REGISTER
+          </button>
+        </div>
+      </Form>
+    </Formik>
+  );
+}
